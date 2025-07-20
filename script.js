3e72c7c9722ae0a33f1f22464c339701148eccbe
Harold Lam <lamwahhinharold@gmail.com>
2025-07-20 11:24:24 +0800

    Add logic to play single round

--- script.js
+++ script.js
@@ -13,11 +13,45 @@ function getComputerChoice() {
   }
 }
 
-console.log(getComputerChoice());
-
 function getHumanChoice() {
   humanChoice = prompt("What is your choice? Rock, paper or scissors?");
   return humanChoice;
 }
 
-console.log(getHumanChoice());
+function capitalizeFirstLetter(val) {
+  return String(val).charAt(0).toUpperCase() + String(val).slice(1);
+}
+
+function playRound(humanChoice, computerChoice) {
+  let sanitizedHumanChoice = humanChoice.toLowerCase();
+  console.log("Your choice: " + capitalizeFirstLetter(sanitizedHumanChoice));
+  console.log("Computer's choice: " + capitalizeFirstLetter(computerChoice));
+  if (sanitizedHumanChoice === "paper" && computerChoice === "rock") {
+    console.log("You win! Paper beats Rock");
+    humanScore++;
+  } else if (
+    sanitizedHumanChoice === "scissors" &&
+    computerChoice === "paper"
+  ) {
+    console.log("You win! Scissors beats Paper");
+    humanScore++;
+  } else if (sanitizedHumanChoice === "rock" && computerChoice === "scissors") {
+    console.log("You win! Rock beats Scissors");
+    humanScore++;
+  } else if (sanitizedHumanChoice === computerChoice) {
+    console.log(
+      "It's a draw. The computer and you both picked " +
+        capitalizeFirstLetter(sanitizedHumanChoice)
+    );
+  } else {
+    console.log(
+      "You lose! " +
+        capitalizeFirstLetter(computerChoice) +
+        " beats " +
+        capitalizeFirstLetter(sanitizedHumanChoice)
+    );
+    computerScore++;
+  }
+}
+
+playRound(getHumanChoice(), getComputerChoice());
